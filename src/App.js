37acbb085a22adee8e5bc95a7a714b394977ea77
unknown <koreswapnil72@gmail.com>
2022-12-14 09:16:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TextForm from './Components/TextForm';
+
+function App() {
+  return (
+    <div className="App">
+      <TextForm/>
+    </div>
+  );
+}
+
+export default App;
